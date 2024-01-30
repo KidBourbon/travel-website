c451b2a2e4eb9abcb5893b620d848e9d9267181c
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-01-30 18:02:17 -0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-  ],
+  content: ["./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
