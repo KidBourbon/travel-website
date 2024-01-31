ecd608313a31171dc54c6606e951f2e3d92113fe
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-01-31 00:57:40 -0500

    Added core plugins: - backgroundImages - colors - fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bali: "url(./img/bali.jpg)",
+        chicago: "url(./img/chicago.jpg)",
+        europe: "url(./img/europe.jpg)",
+        iceland: "url(./img/iceland.jpg)",
+        LA: "url(./img/LA.jpg)",
+        miami: "url(./img/miami.jpg)",
+        new_york: "url(./img/new_york.jpg)",
+        norway: "url(./img/norway.jpg)",
+        sanFrancisco: "url(./img/sanFrancisco.jpg)",
+        sanFranciscoDesktop: "url(./img/sanFranciscoDesktop.jpg)",
+        seattle: "url(./img/seattle.jpg)",
+        switzerland: "url(./img/switzerland.jpg)",
+        sydney: "url(./img/sydney.jpg)",
+        yosemite: "url(./img/yosemite.jpg)",
+      },
+      colors: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
